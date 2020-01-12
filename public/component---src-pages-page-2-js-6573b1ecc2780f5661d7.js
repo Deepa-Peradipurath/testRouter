a21abe313c80571b66fc56ef4f3aa5e2f07db495
Deepa-Peradipurath <deepa.peradipurath@gmail.com>
2020-01-12 18:06:32 +0530

    testRouter

---
+++ public/component---src-pages-page-2-js-6573b1ecc2780f5661d7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("vrFN");t.default=function(){return o.a.createElement("div",null,o.a.createElement(c.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-6573b1ecc2780f5661d7.js.map
