02600f04b6407f960a5593bf4faca2c0399a418c
Deepa P <deepa.P6@cognizant.com>
2020-06-25 14:24:16 +0530

    header change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <div>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>PUSH NOTIFICATIONS POC</h1>
+
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
