25d8f23c9a0ba8cbf3d0dc495f8eba37e9209924
Deepa P <deepa.P6@cognizant.com>
2020-06-25 17:16:11 +0530

    build with static

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <div>
     <SEO title="Home" />
-    <h1>PUSH NOTIFICATIONS POC</h1>
+    <h1>PUSH NOTIFICATIONS POC 2</h1>
 
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
